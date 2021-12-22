6dc6096c8fb5b8b4aa57edbe4228a01d8937e9dd
Dmitrii <35778260+mithridata-com@users.noreply.github.com>
2021-12-23 00:35:19 +0300

    3

--- FM_CODE_MONTHLY_DATA.R
+++ FM_CODE_MONTHLY_DATA.R
@@ -1,39 +1,35 @@
 library(RCurl)
 library(XML)
-library(readxl)
+library(openxlsx)
 library(dplyr)
 library(forecast)
 library(zoo)
+library(httr)
+library(curl)
 
-url <- "https://www.cbr.ru/hd_base/mkr/mkr_base/?UniDbQuery.Posted=True&UniDbQuery.From=01.08.2000&UniDbQuery.To=31.12.2021&UniDbQuery.st=SF&UniDbQuery.ob=OB_MIACR_0&UniDbQuery.Currency=1&UniDbQuery.sk=Dd1_"
-parse_url <- getURL(url,.opts = list(ssl.verifypeer = FALSE) )
-data.raw <- readHTMLTable(parse_url)[[1]]
-colnames(data.raw) <- c("Date","MIACR")
-data.raw$Date <- as.Date(data.raw$Date, format = "%d.%m.%Y")
-data.raw$MIACR <- as.numeric(gsub(",",".",data.raw$MIACR))
+urlfile<-"https://raw.githubusercontent.com/mithridata-com/NOVAIMS_FM_project/main/mortgage_data.csv"
+mortgage <- read.csv2( curl(urlfile) )
+colnames(mortgage) <- c("REPORT_DATE","QUANTITY","VOLUME")
+mortgage$QUANTITY <- as.numeric(gsub(" ","",mortgage$QUANTITY))
+mortgage$VOLUME <- as.numeric(gsub(" ","",mortgage$VOLUME))
 
-full.ts <- data.frame("Date" = seq(from = as.Date('2000-08-01'),to = as.Date('2021-12-16'),by = "day"))
-data.ts <- full.ts %>% 
-  left_join(data.raw, by = c("Date")) %>%
-  na.locf(fromLast = TRUE)
+par(mfrow = c(3,1))
+ts.q <- ts(mortgage$QUANTITY, 
+         start = c(2009,01),
+         end = c(2021,10),
+         frequency = 12)
+ts.q
 
-ts <- ts(data.ts$MIACR)
-ts
+plot(ts.q, main = "TS QUANTITY OF MORTGAGE PER MONTH")
+acf(ts.q, main = "ACF")
+pacf(ts.q, main = "PACF")
 
-ts <- ts(data.ts$MIACR, 
-         start = c(2000,08),
-         end = c(2021,12),
-         frequency = 365)
-ts
-plot(ts)
-# time series is ready. Let's analyse it
-acf(ts)
-pacf(ts)
-decompose(ts)
-plot(decompose(ts))
+ts.v <- ts(mortgage$VOLUME, 
+           start = c(2009,01),
+           end = c(2021,10),
+           frequency = 12)
+ts.v
 
-auto.arima(ts)
-
-ts2 <- diff(ts)
-plot(ts2)
-tail(ts2)
+plot(ts.v, main = "TS VOLUME OF MORTGAGE PER MONTH")
+acf(ts.v, main = "ACF")
+pacf(ts.v, main = "PACF")
